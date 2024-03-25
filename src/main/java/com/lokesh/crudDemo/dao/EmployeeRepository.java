921daac517b9a20c2716b788030dc04c39d3fb71
lokeshnaik-01 <lavadiyalokeshnaik@gmail.com>
2024-03-25 19:36:28 +0530

    spring data rest configs

--- src/main/java/com/lokesh/crudDemo/dao/EmployeeRepository.java
+++ src/main/java/com/lokesh/crudDemo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.lokesh.crudDemo.dao;
 
 import com.lokesh.crudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// instead of /employees spring will expose to /members
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // all crud method will be present
 
